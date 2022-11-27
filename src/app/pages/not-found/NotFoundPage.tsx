20bb89bc91e5f7dc7a184d3277da28cda46718f0
Ugo Venture <venture.ugo@gmail.com>
2022-11-27 15:35:56 +0100

    add notfound page

---
+++ src/app/pages/not-found/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './not-found.scss';
+
+export const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>Not Found</h1>
+        </div>
+    );
+};
