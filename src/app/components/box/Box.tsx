7bce8fec34868d3bcb7bb8fbbd9725745c053806
Ugo Venture <venture.ugo@gmail.com>
2022-11-27 20:59:20 +0100

    edit main page

--- src/app/components/box/Box.tsx
+++ src/app/components/box/Box.tsx
@@ -10,6 +10,7 @@ interface BoxProps {
 export const Box = ({ title, imageUrl }: BoxProps) => {
     return (
         <div className="box-container">
+            <div className='box-background'></div>
             {imageUrl ? <img src={imageUrl} alt={title} /> : <div className='box-overlay'></div>}
             <p className="box-title">{title}</p>
         </div>
--- src/app/pages/home/HomePage.tsx
+++ src/app/pages/home/HomePage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Banner } from '../../components/banner/Banner';
+import { Box } from '../../components/box/Box';
 
 import './home.scss';
 
 export const HomePage = () => {
     return (
         <div className="home-container">
-            <h1>Home Page</h1>
             <Banner title="Chez vous, partout et ailleurs" imgUrl="https://picsum.photos/800/200"/>
+
+            <section className="cards-container">
+                { [...Array(6)].map((_, i) => <Box title="Titre de la location" key={i} />) }
+            </section>
         </div>
     );
 };
