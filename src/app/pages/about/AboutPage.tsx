a5d4fd723dbe330e5c03b153987a51d88bd6692a
Ugo Venture <venture.ugo@gmail.com>
2022-11-27 15:32:56 +0100

    create about route

---
+++ src/app/pages/about/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import './about.scss';
+
+export const AboutPage = () => {
+    return (
+        <div className="about">
+            <h1>About</h1>
+        </div>
+    );
+}
--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -5,16 +5,24 @@ import { Header } from '../components/header/Header';
 import { Footer } from '../components/footer/Footer';
 
 import { HomePage } from '../pages/home/HomePage';
+import { AboutPage } from '../pages/about/AboutPage';
 
 import './app-router.scss';
 
-export const AppRouter = () => {
+export enum AppRoute {
+    Home = '/',
+    About = '/about',
+    NotFound = '/not-found',
+}
 
+export const AppRouter = () => {
     return (
         <Router>
             <Header />
             <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path={AppRoute.Home} element={<HomePage />} />
+                <Route path={AppRoute.About} element={<AboutPage />} />
+                <Route path={AppRoute.NotFound} element={<div>Not Found</div>} />
                 <Route path="*" element={<div>Not Found</div>} />
             </Routes>
             <Footer />
